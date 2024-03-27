37ef7b441bda1c7859644dff7ac264ef756e0e18
Mavlon4658 <72940597+Mavlon4658@users.noreply.github.com>
2024-03-27 07:53:50 +0500

    the pages is ready"

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
     
-
+    $('.change_theme__btn').click(function () {
+        $('.change_theme ul').slideToggle(300);
+        $(this).toggleClass('active')
+    })
     
 
     
